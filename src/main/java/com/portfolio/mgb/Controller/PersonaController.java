76a718d7c61e55b4da857768f88ab1aef015170a
Cielo <croccella@dc.uba.ar>
2022-10-26 18:41:05 -0300

    .

--- src/main/java/com/portfolio/mgb/Controller/PersonaController.java
+++ src/main/java/com/portfolio/mgb/Controller/PersonaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 //@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "hhttps://frontend-f452c.web.app")
+@CrossOrigin(origins = "https://frontend-f452c.web.app")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
