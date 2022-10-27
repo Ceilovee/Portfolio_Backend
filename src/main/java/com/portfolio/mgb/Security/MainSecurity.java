25f5feb6146a6a4a4807b90f84772ce0356b0782
Cielo <croccella@dc.uba.ar>
2022-10-26 23:09:56 -0300

    espero q este andando

--- src/main/java/com/portfolio/mgb/Security/MainSecurity.java
+++ src/main/java/com/portfolio/mgb/Security/MainSecurity.java
@@ -8,6 +8,8 @@ package com.portfolio.mgb.Security;
 import com.portfolio.mgb.Security.Service.UserDetailsImpl;
 import com.portfolio.mgb.Security.jwt.JwtEntryPoint;
 import com.portfolio.mgb.Security.jwt.JwtTokenFilter;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +23,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -41,19 +44,43 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
-                .authorizeRequests()
-                .antMatchers("**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);           
+
+
+        List<String> list1 = Arrays.asList(new String[]{"Authorization", "Cache-Control", "Content-Type"});
+        List<String> list2 = Arrays.asList(new String[]{"http"});
+        List<String> list3 = Arrays.asList(new String[]{"GET", "POST", "PUT", "DELETE", "OPTIONS"});
+        List<String> list4 = Arrays.asList(new String[]{"Authorization"});
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(list1);
+        corsConfiguration.setAllowedOrigins(list2);
+        corsConfiguration.setAllowedMethods(list3);
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setExposedHeaders(list4);
+
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("**").permitAll();
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.cors().configurationSource(request -> corsConfiguration);
+        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
     }
+
+    //@Override
+    //protected void configure(HttpSecurity http) throws Exception {
+    //    http.cors().and().csrf().disable()
+    //            .authorizeRequests()
+    //            .antMatchers("**").permitAll()
+    //            .anyRequest().authenticated()
+    //            .and()
+    //            .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
+    //            .and()
+    //            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    //    http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);           
+    //}
 
     @Override
     protected AuthenticationManager authenticationManager() throws Exception {
